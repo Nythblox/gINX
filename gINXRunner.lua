12c484552a733584b239aa988d887a2050976ff9
Nythblox <nythblox@gmail.com>
2025-05-30 07:26:33 -0700

    Create gINXRunner.lua

---
+++ gINXRunner.lua
@@ -0,0 +1,5 @@
+if isfile("Ginx.lua") then
+  delfile("Ginx.lua")
+end
+local contents = game:HttpGet("https://raw.githubusercontent.com/Nythblox/gINX/refs/heads/main/script.lua", true)
+writefile("Ginx.lua", contents)
